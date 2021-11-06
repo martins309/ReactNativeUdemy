69b3eb9e78e82515fddb230af321a626f22b69a7
Andrew Martins <andrew_omartins@yahoo.com>
2021-11-06 14:21:37 -0400

    added call back function to the color counter component

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
-import { useState } from "react";
 import ColorCounter from "./Components/ColorCounter";
 
 const SquareScreen = () => {
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+
+  console.log(red);
+
   return (
     <View>
-      <ColorCounter color="Red" />
-      <ColorCounter color="Blue"/>
-      <ColorCounter color="Green"/>
+      <ColorCounter
+        onIncrease={() => setRed(red + 1)}
+        onDecrease={() => setRed(red - 1)}
+        color="Red"
+      />
+      <ColorCounter color="Blue" />
+      <ColorCounter color="Green" />
     </View>
   );
 };
