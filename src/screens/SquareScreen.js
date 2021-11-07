b77f271348067503424588b1c832ff9b69256640
Andrew Martins <andrew_omartins@yahoo.com>
2021-11-07 14:08:00 -0500

    created the reducer function and will wire it to the component tomorrow

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,66 +1,54 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import ColorCounter from "./Components/ColorCounter";
 
+
+
+
+const reducer = (state, action) => {
+  // state === { red: number, green: number, blue: number}
+  // action === { colorToChange: 'red', 'green', or 'blue' }
+
+  switch (action.colorToChange) {
+    case 'red':
+      return{ ...state, red: state.red + action.amount };
+    case 'green':
+      return{...state, green: state.green + action.amount };
+    case 'blue':
+      return{...state, blue: state.blue + action.amount };
+    default:
+      return state;
+  }
+};
+
+
 const SquareScreen = () => {
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  const [blue, setBlue] = useState(0);
-
-  const setColor = (color, change) => {
-    //color === 'red', 'green', 'blue'
-    //change is going to be +- 15
-
-    switch (color) {
-      case "red":
-        red + change > 255 || red + change < 0 ? null : setRed(red + change);
-        return;
-
-      case "blue":
-        blue + change > 255 || blue + change < 0
-          ? null
-          : setBlue(blue + change);
-        return;
-
-      case "green":
-        green + change > 255 || green + change < 0
-          ? null
-          : setGreen(green + change);
-        return;
-
-      default:
-        return;
-    }
-  };
-
-  // WHEN YOU WANT TO CREATE TRUE CONSTANT VALUE THAT IS A
-  //CONFIGURATION OPTION AND NOT AS A PROP ALL CAPS
-  //FOR THE VARIABLE
+  
+  
+  
+  
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0})
+  
+ 
 
   const COLOR_INCREMENT = 15;
-
-  console.log(red);
-
-  //on line 32, the two curly braces mean that we are passing an object directly.
-  //first brace is a reference to a javascript object.
-  //second brace is the object literal.
 
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setColor("red", COLOR_INCREMENT)}
-        onDecrease={() => setColor("red", -1 * COLOR_INCREMENT)}
+        onIncrease={() =>}
+        onDecrease={() => }
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => setColor("blue", +COLOR_INCREMENT)}
-        onDecrease={() => setColor("blue", -1 * COLOR_INCREMENT)}
+        onIncrease={() => }
+        onDecrease={() => }
         color="Blue"
       />
 
       <ColorCounter
-        onIncrease={() => setColor("green", +COLOR_INCREMENT)}
-        onDecrease={() => setColor("green", -1 * COLOR_INCREMENT)}
+        onIncrease={() => }
+        onDecrease={() => }
         color="Green"
       />
       <View
