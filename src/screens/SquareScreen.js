ad27f7b353722385e9ab7d871ac16c921ecb4440
Andrew Martins <andrew_omartins@yahoo.com>
2021-11-07 12:44:11 -0500

    created a helper function to exclude negative values

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -7,17 +7,60 @@ const SquareScreen = () => {
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
 
+
+  const setColor = (color, change) => {
+     //color === 'red', 'green', 'blue'
+    //change is going to be +- 15
+
+    if (color === 'red'){
+      if(red + change > 255 || red + change < 0){
+        return;
+    } else {
+      setRed(red + change);
+    }
+  }
+}
+   
+
+    
+  
+  // WHEN YOU WANT TO CREATE TRUE CONSTANT VALUE THAT IS A 
+  //CONFIGURATION OPTION AND NOT AS A PROP ALL CAPS 
+  //FOR THE VARIABLE
+
+  const COLOR_INCREMENT = 15;  
+
   console.log(red);
+
+  //on line 32, the two curly braces mean that we are passing an object directly.
+  //first brace is a reference to a javascript object.
+  //second brace is the object literal.
 
   return (
     <View>
       <ColorCounter
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => setColor('red', COLOR_INCREMENT)}
+        onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
         color="Red"
       />
-      <ColorCounter color="Blue" />
-      <ColorCounter color="Green" />
+      <ColorCounter
+        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
+        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+        color="Blue"
+      />
+
+      <ColorCounter
+        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
+        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+        color="Green"
+      />
+      <View
+        style={{
+          height: 150,
+          width: 150,
+          backgroundColor: `rgb(${red}, ${green}, ${blue} )`,
+        }}
+      />
     </View>
   );
 };
