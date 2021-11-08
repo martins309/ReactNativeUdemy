9364b7bc71d19213de728f01148a6f09335111f9
Andrew Martins <andrew_omartins@yahoo.com>
2021-11-08 16:52:21 -0500

    swtiched the usestate hook for the reducer for practice

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,28 +1,41 @@
-import React, { useState }from 'react';
-import {Text, View, StyleSheet, Button} from 'react-native';
+import React, { useReducer } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
 
+const reducer = (state, action) => {
+  return state.counter + action.payload > 100 ||
+    state.counter + action.payload < 0
+    ? state
+    : { ...state, counter: state.counter + action.payload };
+};
 
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
-   
+  const [state, dispatch] = useReducer(reducer, {
+    counter: 0,
+  });
 
-    return (
-        <View>
-            <Button 
-            title="Increase" 
-            onPress={() => {
-            setCounter(counter + 1);  
-            }}/>
-            <Button 
-            title="Decrease" 
-            onPress={() => {
-            setCounter(counter - 1);
-            }} />
-        <Text>Current Count:{counter}</Text>
+  const COUNTER_INCREMENT = 5;
+
+  const counter = state;
+
+  return (
+    <View>
+      <Button
+        title="Increase"
+        onPress={() => {
+          dispatch({ type: state, payload: COUNTER_INCREMENT });
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          dispatch({ type: state, payload: -1 * COUNTER_INCREMENT });
+        }}
+      />
+      <Text>Current Count:{state.counter}</Text>
     </View>
-    );
-}
-    
-const styles = StyleSheet.create({})
+  );
+};
 
-    export default CounterScreen;
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
