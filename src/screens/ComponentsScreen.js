1ea25a85b0c798970afc0d62bda9dbfd17891e69
Andrew Martins <andrew_omartins@yahoo.com>
2022-05-19 23:21:19 -0400

    first exercise with using react native elements

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,18 +2,21 @@ import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-   const greeting = <Text>What is this happenin g to you</Text> 
+  const greeting = <Text>My name is Andrew</Text>;
   return (
     <View>
-      <Text style={styles.textStyle}>Why am I like this??</Text>
-     {greeting}
+      <Text style={styles.textStyle}>Getting Started with react native</Text>
+      <Text style={styles.textGreeting}>{greeting}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30,
+    fontSize: 45,
+  },
+  textGreeting: {
+    fontSize: 20,
   },
 });
 
