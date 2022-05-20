32ef947b0e3a8f914725b8b5f1ec82083b0a2156
Andrew Martins <andrew_omartins@yahoo.com>
2022-05-19 23:56:23 -0400

    when building lists, use flatlist element, assign the data prop to friends array, then use the renderitem function to return those items. Use keys as performance optimization to keep track of every item in a list

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,22 +2,23 @@ import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-  const greeting = <Text>My name is Andrew</Text>;
-  return (
-    <View>
-      <Text style={styles.textStyle}>Getting Started with react native</Text>
-      <Text style={styles.textGreeting}>{greeting}</Text>
-    </View>
-  );
+    const greeting = 'face here'
+ return (
+     <View>
+         <Text style={styles.text}>Hi there</Text>
+         <Text style={styles.subText}>{greeting}</Text>
+     </View>
+ )
 };
 
 const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 45,
-  },
-  textGreeting: {
-    fontSize: 20,
-  },
+    text: {
+        fontSize: 30
+    },
+    subText: {
+        fontSize: 30
+    }
+
 });
 
 export default ComponentsScreen;
---
+++ src/screens/ListScreen.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { View, Text, StyleSheet, FlatList} from 'react-native'
+
+//whenever we want to build a list in react native we use the flat list element
+//The starting point is always an array of something
+//Array of objects, strings, and etc..
+//normally we would use the map function but in rn we have the flatlist to display lists in a performant fasion
+//element has an item:{name: "friend 1"}
+//instead pull of the item property and destructure item 
+//add a key so that react native doesnt have to rebuild list when the component re renders
+
+const ListScreen = () => {
+    const friends = [
+        {name: 'friend 1'},
+        {name: 'friend 2'},
+        {name: 'friend 3'},
+        {name: 'friend 4'}
+    ]
+    return (
+        <FlatList 
+        data={friends} 
+        renderItem={({ item }) => {
+            return<Text>{item.name}</Text>
+        }}/>
+    )
+}
+
+export default ListScreen
