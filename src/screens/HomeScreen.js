b90df92539d34630096d28117dd6665f6b05bf23
Andrew Martins <andrew_omartins@yahoo.com>
2022-05-23 02:43:17 -0400

    the difference between button and touchable opacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-
- return (
-  <Text style={styles.text}>whats up hoe  </Text>
-)
-};
+  return (
+    <View>
+      <Text style={styles.text}>
+        whats poppin 
+      </Text>
+      <Button 
+      title="Deez Nuts" 
+      onPress={() => console.log("daddy")}
+      />
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   text: {
