065537294c22448fe443c230621d69da0ec98852
Andrew Martins <andrew_omartins@yahoo.com>
2022-05-23 03:05:31 -0400

    button always requires a title, both elements need an onPress

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
-      <Text style={styles.text}>
-        whats poppin 
-      </Text>
-      <Button 
-      title="Deez Nuts" 
-      onPress={() => console.log("daddy")}
+      <Text style={styles.text}>whats poppin</Text>
+      <Button
+        title="Deez Nuts"
+        onPress={() => navigation.navigate("Components")}
+      />
+      <Button
+        title="Go To list Item"
+        onPress={() => navigation.navigate("List")}
       />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
 
 export default HomeScreen;
-
