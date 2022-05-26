34c7742b983aeeda6a69c486a3fcc8fc9ca0c20b
Andrew Martins <andrew_omartins@yahoo.com>
2022-05-25 23:59:23 -0400

    .

--- src/screens/Components/ImageDetail.js
+++ src/screens/Components/ImageDetail.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 
 const ImageDetail = (props) => {
-  
+  console.log(props)
   return (
     <View>
       <Text>{props.title}</Text>
