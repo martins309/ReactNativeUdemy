eed55a5708e317374ff340a0f7272ab921e1f7ee
Andrew Martins <andrew_omartins@yahoo.com>
2022-05-28 19:54:00 -0400

    added new color component

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { Text, StyleSheet, View} from 'react-native'
 
 const ColorScreen = () => {
+    const [color, setColor] = useState('')
     return (
         <View>
+            <Button title="colorTing" onPress={() => setColor(color)}/>
             <Text> Fuck me sideways</Text>
         </View>
     )
